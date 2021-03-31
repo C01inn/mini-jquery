3110ab42bd24c3e1109a80b917c1eb7b8316e4f0
C01inn <colinnpeters@gmail.com>
2021-03-31 00:36:49 -0400

    first commit

---
+++ jquery.js
@@ -0,0 +1,341 @@
+const cssTable = {
+    "align-content": "alignContent",
+    "align-items": "alignItems",
+    "align-self": "alignSelf",
+    "alignment-baseline": "alignmentBaseline",
+    "all": "all",
+    "animation": "animation",
+    "animation-delay": "animationDelay",
+    "animation-direction": "animationDirection",
+    "animation-duration": "animationDuration",
+    "animation-fill-mode": "animationFillMode",
+    "animation-iteration-count": "animationIterationCount",
+    "animation-name": "animationName",
+    "animation-play-state": "animationPlayState",
+    "animation-timing-function": "animationTimingFunction",
+    "backface-visibility": "backfaceVisibility",
+    "background": "background",
+    "background-attachment": "backgroundAttachment",
+    "background-clip": "backgroundClip",
+    "background-color": "backgroundColor",
+    "background-image": "backgroundImage",
+    "background-origin": "backgroundOrigin",
+    "background-position": "backgroundPosition",
+    "background-repeat": "backgroundRepeat",
+    "background-size": "backgroundSize",
+    "background-blend-mode": "backgroundBlendMode",
+    "baseline-shift": "baselineShift",
+    "border": "border",
+    "border-color": "borderColor",
+    "border-style": "borderStyle",
+    "border-width": "borderWidth",
+    "border-bottom": "borderBottom",
+    "border-bottom-color": "borderBottomColor",
+    "border-bottom-style": "borderBottomStyle",
+    "border-bottom-width": "borderBottomWidth",
+    "border-left": "borderLeft",
+    "border-left-color": "borderLeftColor",
+    "border-left-style": "borderLeftStyle",
+    "border-left-width": "borderLeftWidth",
+    "border-right": "borderRight",
+    "border-right-color": "borderRightColor",
+    "border-right-style": "borderRightStyle",
+    "border-right-width": "borderRightWidth",
+    "border-top": "borderTop",
+    "border-top-color": "borderTopColor",
+    "border-top-style": "borderTopStyle",
+    "border-top-width": "borderTopWidth",
+    "border-collapse": "borderCollapse",
+    "border-image": "borderImage",
+    "border-image-outset": "borderImageOutset",
+    "border-image-repeat": "borderImageRepeat",
+    "border-image-slice": "borderImageSlice",
+    "border-image-source": "borderImageSource",
+    "border-image-width": "borderImageWidth",
+    "border-radius": "borderRadius",
+    "border-bottom-left-radius": "borderBottomLeftRadius",
+    "border-bottom-right-radius": "borderBottomRightRadius",
+    "border-top-left-radius": "borderTopLeftRadius",
+    "border-top-right-radius": "borderTopRightRadius",
+    "border-spacing": "borderSpacing",
+    "bottom": "bottom",
+    "box-shadow": "boxShadow",
+    "box-sizing": "boxSizing",
+    "break-after": "breakAfter",
+    "break-before": "breakBefore",
+    "break-inside": "breakInside",
+    "buffered-rendering": "bufferedRendering",
+    "caption-side": "captionSide",
+    "clear": "clear",
+    "clip": "clip",
+    "clip-path": "clipPath",
+    "clip-rule": "clipRule",
+    "color": "color",
+    "color-interpolation": "colorInterpolation",
+    "color-interpolation-filters": "colorInterpolationFilters",
+    "color-rendering": "colorRendering",
+    "column-fill": "columnFill",
+    "column-gap": "columnGap",
+    "column-rule": "columnRule",
+    "column-rule-color": "columnRuleColor",
+    "column-rule-style": "columnRuleStyle",
+    "column-rule-width": "columnRuleWidth",
+    "column-span": "columnSpan",
+    "columns": "columns",
+    "column-count": "columnCount",
+    "column-width": "columnWidth",
+    "contain": "contain",
+    "content": "content",
+    "counter-increment": "counterIncrement",
+    "counter-reset": "counterReset",
+    "counter-set": "counterSet",
+    "cursor": "cursor",
+    "direction": "direction",
+    "display": "display",
+    "dominant-baseline": "dominantBaseline",
+    "empty-cells": "emptyCells",
+    "fill": "fill",
+    "fill-opacity": "fillOpacity",
+    "fill-rule": "fillRule",
+    "filter": "filter",
+    "float": "float",
+    "flex": "flex",
+    "flex-basis": "flexBasis",
+    "flex-grow": "flexGrow",
+    "flex-shrink": "flexShrink",
+    "flex-flow": "flexFlow",
+    "flex-direction": "flexDirection",
+    "flex-wrap": "flexWrap",
+    "flood-color": "floodColor",
+    "flood-opacity": "floodOpacity",
+    "font": "font",
+    "font-family": "fontFamily",
+    "font-size": "fontSize",
+    "font-stretch": "fontStretch",
+    "font-style": "fontStyle",
+    "font-weight": "fontWeight",
+    "font-feature-settings": "fontFeatureSettings",
+    "font-kerning": "fontKerning",
+    "font-variant": "fontVariant",
+    "font-variant-caps": "fontVariantCaps",
+    "font-variant-east-asian": "fontVariantEastAsian",
+    "font-variant-ligatures": "fontVariantLigatures",
+    "font-variant-numeric": "fontVariantNumeric",
+    "grid": "grid",
+    "grid-auto-flow": "gridAutoFlow",
+    "grid-auto-columns": "gridAutoColumns",
+    "grid-auto-rows": "gridAutoRows",
+    "grid-template": "gridTemplate",
+    "grid-template-areas": "gridTemplateAreas",
+    "grid-template-columns": "gridTemplateColumns",
+    "grid-template-rows": "gridTemplateRows",
+    "grid-area": "gridArea",
+    "grid-column": "gridColumn",
+    "grid-column-start": "gridColumnStart",
+    "grid-column-end": "gridColumnEnd",
+    "grid-row": "gridRow",
+    "grid-row-start": "gridRowStart",
+    "grid-row-end": "gridRowEnd",
+    "height": "height",
+    "hyphens": "hyphens",
+    "image-orientation": "imageOrientation",
+    "image-rendering": "imageRendering",
+    "isolation": "isolation",
+    "justify-content": "justifyContent",
+    "justify-items": "justifyItems",
+    "justify-self": "justifySelf",
+    "left": "left",
+    "letter-spacing": "letterSpacing",
+    "lighting-color": "lightingColor",
+    "line-break": "lineBreak",
+    "line-height": "lineHeight",
+    "list-style": "listStyle",
+    "list-style-image": "listStyleImage",
+    "list-style-position": "listStylePosition",
+    "list-style-type": "listStyleType",
+    "margin": "margin",
+    "margin-bottom": "marginBottom",
+    "margin-left": "marginLeft",
+    "margin-right": "marginRight",
+    "margin-top": "marginTop",
+    "marker": "marker",
+    "marker-end": "markerEnd",
+    "marker-mid": "markerMid",
+    "marker-start": "markerStart",
+    "mask": "mask",
+    "mask-type": "maskType",
+    "max-height": "maxHeight",
+    "max-width": "maxWidth",
+    "min-height": "minHeight",
+    "min-width": "minWidth",
+    "mix-blend-mode": "mixBlendMode",
+    "object-fit": "objectFit",
+    "object-position": "objectPosition",
+    "opacity": "opacity",
+    "order": "order",
+    "orphans": "orphans",
+    "outline": "outline",
+    "outline-color": "outlineColor",
+    "outline-style": "outlineStyle",
+    "outline-width": "outlineWidth",
+    "outline-offset": "outlineOffset",
+    "overflow": "overflow",
+    "overflow-x": "overflowX",
+    "overflow-y": "overflowY",
+    "overflow-wrap": "overflowWrap",
+    "padding": "padding",
+    "padding-bottom": "paddingBottom",
+    "padding-left": "paddingLeft",
+    "padding-right": "paddingRight",
+    "padding-top": "paddingTop",
+    "page": "page",
+    "page-break-after": "pageBreakAfter",
+    "page-break-before": "pageBreakBefore",
+    "page-break-inside": "pageBreakInside",
+    "paint-order": "paintOrder",
+    "perspective": "perspective",
+    "perspective-origin": "perspectiveOrigin",
+    "pointer-events": "pointerEvents",
+    "position": "position",
+    "quotes": "quotes",
+    "resize": "resize",
+    "right": "right",
+    "ruby-position": "rubyPosition",
+    "scroll-behavior": "scrollBehavior",
+    "scroll-snap-type": "scrollSnapType",
+    "shape-image-threshold": "shapeImageThreshold",
+    "shape-margin": "shapeMargin",
+    "shape-outside": "shapeOutside",
+    "shape-rendering": "shapeRendering",
+    "size": "size",
+    "speak": "speak",
+    "stop-color": "stopColor",
+    "stop-opacity": "stopOpacity",
+    "stroke": "stroke",
+    "stroke-dasharray": "strokeDasharray",
+    "stroke-dashoffset": "strokeDashoffset",
+    "stroke-linecap": "strokeLinecap",
+    "stroke-linejoin": "strokeLinejoin",
+    "stroke-miterlimit": "strokeMiterlimit",
+    "stroke-opacity": "strokeOpacity",
+    "stroke-width": "strokeWidth",
+    "tab-size": "tabSize",
+    "table-layout": "tableLayout",
+    "text-align": "textAlign",
+    "text-align-last": "textAlignLast",
+    "text-anchor": "textAnchor",
+    "text-combine-upright": "textCombineUpright",
+    "text-decoration": "textDecoration",
+    "text-decoration-color": "textDecorationColor",
+    "text-decoration-line": "textDecorationLine",
+    "text-decoration-style": "textDecorationStyle",
+    "text-indent": "textIndent",
+    "text-orientation": "textOrientation",
+    "text-overflow": "textOverflow",
+    "text-rendering": "textRendering",
+    "text-shadow": "textShadow",
+    "text-size-adjust": "textSizeAdjust",
+    "text-transform": "textTransform",
+    "text-underline-position": "textUnderlinePosition",
+    "top": "top",
+    "touch-action": "touchAction",
+    "transform": "transform",
+    "transform-box": "transformBox",
+    "transform-origin": "transformOrigin",
+    "transform-style": "transformStyle",
+    "transition": "transition",
+    "transition-delay": "transitionDelay",
+    "transition-duration": "transitionDuration",
+    "transition-property": "transitionProperty",
+    "unicode-bidi": "unicodeBidi",
+    "vector-effect": "vectorEffect",
+    "vertical-align": "verticalAlign",
+    "visibility": "visibility",
+    "white-space": "whiteSpace",
+    "widows": "widows",
+    "width": "width",
+    "will-change": "willChange",
+    "word-break": "wordBreak",
+    "word-spacing": "wordSpacing",
+    "word-wrap": "wordWrap",
+    "writing-mode": "writingMode",
+    "z-index": "zIndex"
+}
+
+function fade(type, elem, ms, prop) {
+    let  isIn = type === 'in'
+        let opacity = isIn ? 0 : 1
+        let interval = 50
+        let duration = ms
+        let gap = interval / duration
+        let self = elem;
+    
+      if(isIn) {
+        if (prop === undefined || prop === null) {
+            self.style.display = 'block';
+        } else {
+            self.style.display = prop
+        }
+
+        self.style.opacity = opacity;
+      }
+    
+      function func() {
+        opacity = isIn ? opacity + gap : opacity - gap;
+        self.style.opacity = opacity;
+    
+        if(opacity <= 0) self.style.display = 'none'
+        if(opacity <= 0 || opacity >= 1) window.clearInterval(fading);
+      }
+    
+      var fading = window.setInterval(func, interval);
+}
+
+class jQueryElement {
+    
+    constructor(element) {
+        this.elem = element;
+    }
+    // hide function
+    hide() {
+        this.elem.style.display = 'none'
+    }
+    // show function
+    show(displayProp) {
+        let dis = 'block'
+        if (displayProp !== undefined && displayProp !== null ){
+            dis = displayProp
+        }
+        this.elem.style.display = dis;
+        this.elem.style.visibility = 'visible';
+    }
+    css(prop, value) {
+        let good = cssTable[prop]
+        console.log(good, value)
+        this.elem.style[good] = value;
+    }
+
+    fadeIn(ms, prop) {
+        fade('in', this.elem, ms, prop)
+    }
+    fadeOut(ms, prop) {
+        fade("out", this.elem, ms, prop)
+    }
+    html(markup) {
+        if (markup === undefined || markup === null) {
+            return this.elem.innerHtml;
+        }
+        this.elem.innerHtml = markup;
+    }
+    attr(name, value) {
+        this.elem.setAttribute(name, value);
+    }
+
+}
+
+
+
+// main $ function
+const $ = (selector) => {
+    return new jQueryElement(document.querySelector(selector))
+}
